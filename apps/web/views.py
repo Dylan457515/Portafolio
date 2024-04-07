cf8aa7130a9932168af2d88fa79fdfa3eddb6dca
Dylan <dylanquispehuallpa80@gmail.com>
2024-04-07 16:38:19 -0400

    ajuste de colores y signacion de datos

--- apps/web/views.py
+++ apps/web/views.py
@@ -11,8 +11,15 @@ class index(TemplateView):
         #agregando datos para el portafolio
 
         #agregando informacion basica
-        context['nombre'] = 'Dylan'
+        context['nombre'] = ' Dylan'
         context['apellido'] = ' Quispe Huallpa'
         context['gmail'] = ' dylanquispehuallpa80@gmail.com'
         context['numero'] = '+591 67046908'
+        context['ubicacion'] = 'El Alto · La Paz · Bolivia ·'
+
+        #contenido interno
+        context['presentacion'] = 'Tengo experiencia en aprovechar marcos ágiles para proporcionar una sinopsis sólida para descripciones generales de alto nivel. Los enfoques iterativos de la estrategia corporativa fomentan el pensamiento colaborativo para promover la propuesta de valor general.'
+
+
+
         return context
