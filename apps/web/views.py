4b497e6ab9b31f7d576ce4f39a3c2aa225cf37fd
Dylan <dylanquispehuallpa80@gmail.com>
2024-04-07 18:30:48 -0400

    agregando lenguaje de programacion y encabesado(Cambio N4)

--- apps/web/views.py
+++ apps/web/views.py
@@ -21,7 +21,7 @@ class index(TemplateView):
 
 
         #contenido interno
-        context['presentacion'] = 'Tengo experiencia en aprovechar marcos ágiles para proporcionar una sinopsis sólida para descripciones generales de alto nivel. Los enfoques iterativos de la estrategia corporativa fomentan el pensamiento colaborativo para promover la propuesta de valor general.'
+        context['presentacion'] = 'Mi formación académica me ha equipado con un conocimiento profundo de los principios de la programación y las últimas tecnologías. A través de proyectos universitarios y colaboraciones en equipo, he desarrollado una sólida comprensión de los marcos de desarrollo ágil y las metodologías de programación estructurada. Estoy preparado para aplicar estas habilidades en un entorno profesional, contribuyendo con ideas innovadoras y soluciones eficientes a los desafíos técnicos.'
 
 
 
