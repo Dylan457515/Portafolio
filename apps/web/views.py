f8ddd8f3dfb60b45b44fc83a6c615420f683df36
Dylan <dylanquispehuallpa80@gmail.com>
2024-04-08 23:24:55 -0400

    Se agrego informacion de estudios(Cambio N5)

--- apps/web/views.py
+++ apps/web/views.py
@@ -18,7 +18,10 @@ class index(TemplateView):
         context['ubicacion'] = 'El Alto · La Paz · Bolivia ·'
 
 
-
+        #educasion
+        context['lugar_estudios']= 'Universidad Saleciana de Bolivia'
+        context['carrera'] = 'Ingenieria de Sistemas'
+        context['seguimiento'] = 'August 2006 - Presente'
 
         #contenido interno
         context['presentacion'] = 'Mi formación académica me ha equipado con un conocimiento profundo de los principios de la programación y las últimas tecnologías. A través de proyectos universitarios y colaboraciones en equipo, he desarrollado una sólida comprensión de los marcos de desarrollo ágil y las metodologías de programación estructurada. Estoy preparado para aplicar estas habilidades en un entorno profesional, contribuyendo con ideas innovadoras y soluciones eficientes a los desafíos técnicos.'
